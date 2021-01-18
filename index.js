42fd81fdcf7b0f77ae913751f730332d55738976
rajesh886 <57416451+rajesh886@users.noreply.github.com>
2021-01-18 15:01:12 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var mongoose = require("mongoose");
+var env = require('dotenv').config();
+
+mongoose.connect("mongodb://" + process.env.COSMOSDB_HOST + ":" + process.env.COSMOSDB_PORT + "/" + process.env.COSMOSDB_DBNAME + "?ssl=true&replicaSet=globaldb", {
+    auth: {
+        user: process.env.COSMOSDB_USER,
+        password: process.env.COSMOSDB_PASSWORD
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    retryWrites: false
+}).then(() => console.log('Connection to CosmosDB successful')).catch((err) => console.error(err));
+
+const Family = mongoose.model('Family', new mongoose.Schema({
+    lastName: String,
+    parents: [{
+        familyName: String,
+        firstName: String,
+        gender: String
+    }],
+    children: [{
+        familyName: String,
+        firstName: String,
+        gender: String,
+        grade: Number
+    }],
+    pets: [{
+        givenName: String
+    }],
+    address: {
+        country: String,
+        state: String,
+        city: String
+    }
+}));
+
+const family = new Family({
+    lastName: "Volum",
+    parents: [
+        { firstName: "Thomas" },
+        { firstName: "Mary Kay" }
+    ],
+    children: [
+        { firstName: "Ryan", gender: "male", grade: 8 },
+        { firstName: "Patrick", gender: "male", grade: 7 }
+    ],
+    pets: [
+        { givenName: "Buddy" }
+    ],
+    address: { country: "USA", state: "WA", city: "Seattle" }
+});
+
+family.save((err, saveFamily) => {
+    console.log(JSON.stringify(saveFamily));
+});
