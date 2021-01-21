07be97224c76682e23c195143c3ead9bb197d1ca
rajesh886 <57416451+rajesh886@users.noreply.github.com>
2021-01-21 00:54:41 -0600

    Added OAuth - Unable to issue tokens

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //calling the express function from the express library
 var express = require('express');
-
 var mongoose = require("mongoose");
 var env = require('dotenv').config();
+var passport = require('passport');
+var AzureAdOAuth2Strategy = require('passport-azure-ad-oauth2');
+var parser = require('body-parser');
 
 mongoose.connect("mongodb://" + process.env.COSMOSDB_HOST + ":" + process.env.COSMOSDB_PORT + "/" + process.env.COSMOSDB_DBNAME + "?ssl=true&replicaSet=globaldb", {
     auth: {
@@ -15,6 +17,8 @@ mongoose.connect("mongodb://" + process.env.COSMOSDB_HOST + ":" + process.env.CO
 }).then(() => console.log('Connection to CosmosDB successful')).catch((err) => console.error(err));
 
 const app = express();
+
+app.use(parser.urlencoded({ extended: true }));
 
 //code and discard to test the cosmosDB
 // const Family = mongoose.model('Family', new mongoose.Schema({
@@ -60,7 +64,33 @@ const app = express();
 //     console.log(JSON.stringify(saveFamily));
 // });
 
+passport.use(new AzureAdOAuth2Strategy({
+    clientID: '0c09e3c1-b11d-40ab-9119-d87fb3e87cdc',
+    clientSecret: '8312546a-f9e7-4c5e-9b7b-f764d992d2bf',
+    callbackURL: 'https://localhost:5000',
+    resource: '00000002-0000-0000-c000-000000000000',
+    tenant: 'contoso.onmicrosoft.com',
+    useCommonEndpoint: 'https://login.windows.net/common'
+},
+    function (accessToken, refresh_token, params, profile, done) {
+        var waadProfile = profile || jwt.decode(params.id_token, '', true);
+
+        User.findOrCreate({ id: waadProfile.upn }, function (err, user) {
+            done(err, user);
+        });
+    }));
+
 app.get('/', (req, res) => res.send('Route is working'));
+
+app.get('/auth/azureadoauth2',
+    passport.authenticate('azure_ad_oauth2'));
+
+app.get('/auth/azureadoauth2/callback',
+    passport.authenticate('azure_ad_oauth2', { failureRedirect: '/' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
 
 //Define Routes here
 app.use('/api/auth', require('./routes/api/auth'));
