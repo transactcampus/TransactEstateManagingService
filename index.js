205f889592149929a5801668077f7ea56c7b135e
rajesh886 <57416451+rajesh886@users.noreply.github.com>
2021-01-20 20:11:05 -0600

    Added Authentication Route

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//calling the express function from the express library
+var express = require('express');
+
 var mongoose = require("mongoose");
 var env = require('dotenv').config();
 
@@ -11,45 +14,57 @@ mongoose.connect("mongodb://" + process.env.COSMOSDB_HOST + ":" + process.env.CO
     retryWrites: false
 }).then(() => console.log('Connection to CosmosDB successful')).catch((err) => console.error(err));
 
-const Family = mongoose.model('Family', new mongoose.Schema({
-    lastName: String,
-    parents: [{
-        familyName: String,
-        firstName: String,
-        gender: String
-    }],
-    children: [{
-        familyName: String,
-        firstName: String,
-        gender: String,
-        grade: Number
-    }],
-    pets: [{
-        givenName: String
-    }],
-    address: {
-        country: String,
-        state: String,
-        city: String
-    }
-}));
+const app = express();
 
-const family = new Family({
-    lastName: "Volum",
-    parents: [
-        { firstName: "Thomas" },
-        { firstName: "Mary Kay" }
-    ],
-    children: [
-        { firstName: "Mike", gender: "male", grade: 8 },
-        { firstName: "Patrick", gender: "male", grade: 7 }
-    ],
-    pets: [
-        { givenName: "Buddy" }
-    ],
-    address: { country: "USA", state: "WA", city: "Seattle" }
-});
+//code and discard to test the cosmosDB
+// const Family = mongoose.model('Family', new mongoose.Schema({
+//     lastName: String,
+//     parents: [{
+//         familyName: String,
+//         firstName: String,
+//         gender: String
+//     }],
+//     children: [{
+//         familyName: String,
+//         firstName: String,
+//         gender: String,
+//         grade: Number
+//     }],
+//     pets: [{
+//         givenName: String
+//     }],
+//     address: {
+//         country: String,
+//         state: String,
+//         city: String
+//     }
+// }));
 
-family.save((err, saveFamily) => {
-    console.log(JSON.stringify(saveFamily));
-});
+// const family = new Family({
+//     lastName: "Volum",
+//     parents: [
+//         { firstName: "Thomas" },
+//         { firstName: "Mary Kay" }
+//     ],
+//     children: [
+//         { firstName: "Mike", gender: "male", grade: 8 },
+//         { firstName: "Patrick", gender: "male", grade: 7 }
+//     ],
+//     pets: [
+//         { givenName: "Buddy" }
+//     ],
+//     address: { country: "USA", state: "WA", city: "Seattle" }
+// });
+
+// family.save((err, saveFamily) => {
+//     console.log(JSON.stringify(saveFamily));
+// });
+
+app.get('/', (req, res) => res.send('Route is working'));
+
+//Define Routes here
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
