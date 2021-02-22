75013fe20c7b0ca6c5be402f426661300c9ad2ff
rajesh886 <57416451+rajesh886@users.noreply.github.com>
2021-02-21 23:25:45 -0600

    modified some files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>
+  <Fragment >
+    <h1>App</h1>
+  </Fragment>
+
 
 export default App;
