c267f135b1a8a4f4b5f90a3403cace7b840e3654
rajesh886 <57416451+rajesh886@users.noreply.github.com>
2021-04-01 11:51:39 -0500

    server.js files updated

--- server.js
+++ server.js
@@ -72,9 +72,10 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
+}
 
 
 
-    const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
